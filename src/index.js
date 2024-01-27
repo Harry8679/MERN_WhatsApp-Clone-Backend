0e0701fab896ef0e6666ff099a2b2344a7e53bd1
Harry EMARH <emarh.harry.code@gmail.com>
2024-01-27 20:38:42 +0100

    Cross-Env module added: permit to add a variable in .env file (process.env.NODE_ENV)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import app from "./app.js";
 // dotenv config
 dotenv.config();
 const PORT = process.env.PORT || 6000;
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
   console.log(`WhatsApp Backend is running on the port ${PORT}`);
