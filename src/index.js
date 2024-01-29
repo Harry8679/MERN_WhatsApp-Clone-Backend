f10d34ee36b7088d07a2884c7f0b729bc4c00271
Harry EMARH <emarh.harry.code@gmail.com>
2024-01-29 14:38:24 +0100

    Logger from winston package created in configs folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import logger from './configs/logger.config.js';
 
 const PORT = process.env.PORT || 6000;
 console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(`WhatsApp Backend is running on the port ${PORT}`);
+  logger.info(`WhatsApp Backend is running on the port ${PORT}`);
 });
