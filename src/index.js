dcd57abfa3d2aa39509629d16071db11a76cbb73
Harry EMARH <emarh.harry.code@gmail.com>
2024-01-29 14:56:17 +0100

    Handling uncaughtException, uncaughtRejection errors and SIGTERM signal closing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import app from "./app.js";
-import logger from './configs/logger.config.js';
+import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 6000;
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => {
+let server;
+server = app.listen(PORT, () => {
   logger.info(`WhatsApp Backend is running on the port ${PORT}`);
+  console.log('process id', process.pid);
+//   throw new Error("Error is server");
 });
+
+// Handle server errors
+const exitHandler = () => {
+    if (server) {
+        logger.info('Server closed');
+        process.exit(1);
+    } else {
+        process.exit(1);
+    }
+}
+
+const unexpectedErrorHandler = error => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+// SIGTERM
+process.on('SIGTERM', () => {
+    if (server) {
+        logger.info('Server closed.');
+        process.exit(1);
+    }
+})
