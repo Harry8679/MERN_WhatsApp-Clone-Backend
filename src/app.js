893b5ac6f084dbe20061c02746fc7f4f28370148
Harry EMARH <emarh.harry.code@gmail.com>
2024-01-29 15:59:41 +0100

    Handling http errors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
-import mongoSanitize from 'express-mongo-sanitize';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import fileUpload from 'express-fileupload';
-import cors from 'cors';
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+import createHttpError from "http-errors";
 
 // dotenv config
 dotenv.config();
@@ -31,14 +32,33 @@ app.use(cookieParser());
 // gzip compression
 app.use(compression());
 // file upload
-app.use(fileUpload({
-    useTempFiles: true
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 // cors
 app.use(cors());
 
 app.post("/", (req, res) => {
-  res.send(req.body);
+  throw createHttpError.BadRequest("This route has an error");
+  //   res.send(req.body);
+  // res.status(409).json({ message: 'There is a conflict' });
+});
+
+app.use(async (req, res, next) => {
+    next(createHttpError.NotFound('This route does not exist'));
+});
+
+// Error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
