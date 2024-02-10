87c6fe843f47aef1dfd783f2190d15695c30cbc0
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-10 03:59:51 +0100

    Generate access and refresh tokens, store refresh token in cookies in the server

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -6,7 +7,29 @@ export const register = async (req, res, next) => {
     // res.send(req.body);
     // console.log(name, email, picture, status, password);
     const newUser = await createUser({ name, email, picture, status, password });
-    res.json(newUser);
+
+    const acces_token = await generateToken({ userId: newUser._id }, '30d', process.env.ACCESS_TOKEN_SECRET);
+    const refresh_token = await generateToken({ userId: newUser._id }, '30d', process.env.REFRESH_TOKEN_SECRET);
+
+    res.cookie('refreshtoken', refresh_token, {
+        httpOnly: true,
+        path: '/api/v1/auth/refreshtoken',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    });
+
+    console.table({ acces_token, refresh_token });
+
+    res.json({
+        message: 'Success registration',
+        acces_token,
+        user: {
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            picture: newUser.picture,
+            status: newUser.status,
+        }
+    });
   } catch (error) {
     next(error);
   }
