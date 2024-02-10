bce86762587b7310658b796eb6f416a31b1df3c7
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-10 02:20:52 +0100

    Register a user with validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,31 +1,34 @@
-export const register = async(req, res, next) => {
-    try {
-        res.send(req.body);
-    } catch (error) {
-        next(error);
-    }
-}
+import { createUser } from "../services/auth.service.js";
 
-export const login = async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, picture, status, password } = req.body;
+    // res.send(req.body);
+    // console.log(name, email, picture, status, password);
+    const newUser = await createUser({ name, email, picture, status, password });
+    res.json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const logout = async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+export const login = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const refreshToken = async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+export const logout = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshToken = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
